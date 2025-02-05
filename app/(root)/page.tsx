89847c278762294357ba7c70db15abec408589f2
khanhnqse <khanhqn03@gmail.com>
2025-02-05 12:15:06 +0700

    feat: :sparkles: workshop-list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,10 @@ import SpaceList from "@/components/space-list/space-list";
 import NearSpaceList from "@/components/near-space-list/near-space-list";
 import HighRatingSpace from "@/components/high-rating-space/high-rating-space";
 import SectionTitle from "@/components/ui/section-tilte";
+import Banner from "@/components/ui/banner";
+import WorkshopList from "@/components/workshop-list/workshop-list";
+import FeatureSection from "@/components/ui/feauture-section";
+import { Send } from "lucide-react";
 
 function HomePage() {
   return (
@@ -21,6 +25,53 @@ function HomePage() {
           Không gian làm việc <br /> được đánh giá cao
         </SectionTitle>
         <HighRatingSpace />
+        <div className="space-y-8">
+          <Banner
+            imageUrl="/banner1.png"
+            title="Đăng ký dịch vụ của WorkHive ngay"
+            subtitle="Tăng thêm thu nhập từ không gian của bạn..."
+            buttonText="Trở thành doanh nghiệp ngay"
+            buttonLink="#"
+          />
+        </div>
+        <SectionTitle>Workshop nổi bật</SectionTitle>
+        <WorkshopList />
+        <div className="space-y-8">
+          <Banner
+            imageUrl="/banner2.jpg"
+            title="Tham gia các buổi Workshop"
+            subtitle="Mở rộng kỹ năng, kiến thức với các buổi Workshop đang diễn ra..."
+            buttonText="Tham gia ngay"
+            buttonLink="#"
+          />
+        </div>
+        <FeatureSection
+          title="Khám phá thêm về việc trở thành chủ không gian làm việc"
+          description="Tận dụng tối đa tiềm năng không gian của bạn với nền tảng của chúng tôi..."
+          buttonText="Đăng ký ngay"
+          buttonLink="#"
+          secondaryLinks={[
+            { text: "Tư vấn Hotline", href: "#" },
+            { text: "Nhắn tin Fanpage", href: "#" },
+          ]}
+          imageUrl="/feauture-section.jpg"
+        />
+      </div>
+      <div className="bg-[#7B4A02] py-6 px-6 flex flex-col md:flex-row items-center justify-center gap-6">
+        <div className="text-white text-center md:text-left">
+          <h3 className="text-lg font-bold">Liên hệ ngay</h3>
+          <p className="text-sm opacity-80">Phản hồi nhanh chóng</p>
+        </div>
+        <div className="relative w-full max-w-lg">
+          <input
+            type="email"
+            placeholder="Nhập email..."
+            className="w-full py-3 pl-6 pr-14 rounded-full text-gray-700 placeholder-gray-400 shadow-md focus:outline-none"
+          />
+          <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-gray-400 hover:bg-gray-500 text-white p-3 rounded-full transition">
+            <Send size={18} />
+          </button>
+        </div>
       </div>
     </div>
   );
