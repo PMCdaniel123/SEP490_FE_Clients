a0075d01374caba31104e556bcbe378b0d666efa
khanhnqse <khanhqn03@gmail.com>
2025-02-19 16:45:48 +0700

    feat(notfound): :zap:

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+import type { Metadata } from "next";
+import { ToastContainer } from "react-toastify";
+import Header from "@/components/layout/header";
+import Footer from "@/components/layout/footer";
+import { Providers } from "@/stores/Providers";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "WorkHive",
+  description:
+    "WorkHive was developed as a groundbreaking solution for managing and booking coworking spaces. This platform is designed to provide a professional, centralized, and convenient system, optimizing the management of bookings, organizing information, and supporting events for coworking spaces. WorkHive not only benefits users of these services but also effectively supports coworking space owners in enhancing service quality and expanding their market presence.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={montserrat.className}>
+      <body>
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+          <ToastContainer />
+        </Providers>
+      </body>
+    </html>
+  );
+}
