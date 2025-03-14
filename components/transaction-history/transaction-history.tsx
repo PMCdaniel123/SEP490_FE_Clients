c309159f53ae0a7cc2c4899d5a256ac0ebbbae43
khanhnqse <khanhqn03@gmail.com>
2025-03-15 02:14:47 +0700

    refactor(user-wallet): :sparkles: integration API

--- components/transaction-history/transaction-history.tsx
+++ components/transaction-history/transaction-history.tsx
@@ -1,4 +1,7 @@
 import { ArrowDownCircle, ArrowUpCircle } from "lucide-react";
+import dayjs from "dayjs";
+import { useState } from "react";
+import Pagination from "@/components/pagination/pagination";
 
 interface Transaction {
   id: number;
@@ -19,51 +22,75 @@ const TransactionHistory: React.FC<TransactionHistoryProps> = ({
   transactions,
   formatCurrency,
 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [transactionsPerPage] = useState(5);
+  const sortedTransactions = [...transactions].sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+  const indexOfLastTransaction = currentPage * transactionsPerPage;
+  const indexOfFirstTransaction = indexOfLastTransaction - transactionsPerPage;
+  const currentTransactions = sortedTransactions.slice(
+    indexOfFirstTransaction,
+    indexOfLastTransaction
+  );
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
     <div>
       <h2 className="text-lg font-semibold mb-2">Lịch sử giao dịch</h2>
       <div className="bg-gray-100 p-4 rounded-lg">
         {transactions.length > 0 ? (
-          <ul>
-            {transactions.map((tx) => (
-              <li
-                key={tx.id}
-                className="flex flex-col p-2 border-b last:border-b-0"
-              >
-                <div className="flex justify-between items-center">
-                  <div className="flex items-center gap-2">
-                    {tx.type === "Nạp tiền" ? (
-                      <ArrowDownCircle className="text-green-500" />
-                    ) : (
-                      <ArrowUpCircle className="text-red-500" />
-                    )}
-                    <span>{tx.type}</span>
+          <>
+            <ul>
+              {currentTransactions.map((tx) => (
+                <li
+                  key={tx.id}
+                  className="flex flex-col p-2 border-b last:border-b-0"
+                >
+                  <div className="flex justify-between items-center">
+                    <div className="flex items-center gap-2">
+                      {tx.type === "Nạp tiền" ? (
+                        <ArrowDownCircle className="text-green-500" />
+                      ) : (
+                        <ArrowUpCircle className="text-red-500" />
+                      )}
+                      <span>{tx.type}</span>
+                    </div>
+                    <span>{formatCurrency(tx.amount)}</span>
                   </div>
-                  <span>{formatCurrency(tx.amount)}</span>
-                </div>
-                <div className="flex justify-between items-center mt-2">
-                  <span className="text-gray-500 text-sm">{tx.date}</span>
-                  <span className="text-gray-500 text-sm">
-                    {tx.paymentMethod}
-                  </span>
-                </div>
-                <div className="flex justify-between items-center mt-2">
-                  <span className="text-gray-500 text-sm">
-                    {tx.description}
-                  </span>
-                  <span
-                    className={`text-sm ${
-                      tx.status === "Hoàn thành"
-                        ? "text-green-500"
-                        : "text-red-500"
-                    }`}
-                  >
-                    {tx.status}
-                  </span>
-                </div>
-              </li>
-            ))}
-          </ul>
+                  <div className="flex justify-between items-center mt-2">
+                    <span className="text-gray-500 text-sm">
+                      {dayjs(tx.date).format("DD/MM/YYYY - HH:mm")}
+                    </span>
+                    <span className="text-gray-500 text-sm">
+                      {tx.paymentMethod}
+                    </span>
+                  </div>
+                  <div className="flex justify-between items-center mt-2">
+                    <span className="text-gray-500 text-sm">
+                      {tx.description}
+                    </span>
+                    <span
+                      className={`text-sm ${
+                        tx.status === "Hoàn thành"
+                          ? "text-green-500"
+                          : "text-red-500"
+                      }`}
+                    >
+                      {tx.status}
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+            <Pagination
+              currentPage={currentPage}
+              totalPages={Math.ceil(transactions.length / transactionsPerPage)}
+              onPageChange={paginate}
+            />
+          </>
         ) : (
           <p className="text-gray-500">Chưa có giao dịch nào.</p>
         )}
